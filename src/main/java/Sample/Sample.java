684552a78ccf233b8e494b44e89b110328d6d2b8
user18 <user18@PC18>
2023-07-06 14:15:16 +0900

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I4239288d64d48f45d851bd221aed66fd8b81568f

--- src/main/java/Sample/Sample.java
+++ src/main/java/Sample/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("Main Branch2.");
+		System.out.println("Sub Branch2.");
 		System.out.println("aiueo");
 		System.out.println("test");
 		System.out.println("test1");
