15765d1cb5ba81859496aeacb46062779c85aecd
user18 <user18@PC18>
2023-07-06 13:46:25 +0900

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I002a1f35cb84d1327ae04d912b1c314fa3bbdf85

--- src/main/java/Sample/Sample.java
+++ src/main/java/Sample/Sample.java
@@ -6,7 +6,7 @@ public class Sample {
 		// TODO 自動生成されたメソッド・スタブ
 
 		System.out.println("Main Branch2.");
-		System.out.println("Hello World!");
+		System.out.println("aiueo");
 		System.out.println("test");
 		System.out.println("test1");
 	}
